4abae849829166766e013ea9ca9fd9ff7fc28dd8
zackere <40472142+zackere@users.noreply.github.com>
2018-12-24 17:48:38 +0100

    Add files via upload

---
+++ AiSD_Queue.h
@@ -0,0 +1,42 @@
+#pragma once
+#include <iostream>
+#include "AiSD_List.h"
+using namespace std;
+namespace AiSD
+{
+	template<class T>
+	class Queue
+	{
+		List<T> values;
+	public:
+		Queue() = default;
+		void Push(T elem);
+		T Pop();
+		bool IsEmpty();
+		template<class T>
+		friend ostream& operator<<(ostream &out, const Queue<T> &queue);
+	};
+	template<class T>
+	void Queue<T>::Push(T elem)
+	{
+		this->values->PushBack(elem);
+	}
+	template<class T>
+	T Queue<T>::Pop()
+	{
+		T ret = this->values->GetFront();
+		this->values->PopFront();
+		return ret;
+	}
+	template<class T>
+	bool Queue<T>::IsEmpty()
+	{
+		return this->values->IsEmpty();
+	}
+	template<class T>
+	ostream & operator<<(ostream & out, const Queue<T>& queue)
+	{
+		out << queue.values;
+		return out;
+	}
+}
